991208e8a1369a85504f6865f5e29c5ba9b42c4e
Eric Moreno <emoreno@mit.edu>
2024-10-23 00:10:55 +0200

    Initial commit for Delphes-3.5.0 with local modifications

---
+++ test_config.tcl
@@ -0,0 +1,6 @@
+set InputFile "/eos/project/f/foundational-model-dataset/samples/eric_v1/pu100/tt0123j_5f_ckm_LO_MLM_semiLeptonic-2000-700/tag_1_delphes_events.root"
+set TreeName "Delphes"
+
+set OutputFile "output.root"
+
+source modules/Delphes.tcl
